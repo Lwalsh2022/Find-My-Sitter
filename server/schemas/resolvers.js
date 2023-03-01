906128087c07acd7c3b7a060a2134b07a3c2ad73
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2023-02-28 20:34:36 -0500

    Modified schemas, server, connection

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { Parents, Reviews, Sitters } = require('../models');
+
+const resolvers = {
+  Query: {
+    // schools: async () => {
+    //   return await School.find({}).populate('classes').populate({
+    //     path: 'classes',
+    //     populate: 'professor'
+    //   });
+    // },
+    // classes: async () => {
+    //   return await Class.find({}).populate('professor');
+    // },
+    // professors: async () => {
+    //   return await Professor.find({});
+    // }
+  }
+};
+
+module.exports = resolvers;
