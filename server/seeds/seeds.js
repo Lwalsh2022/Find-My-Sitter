ab042dd47bd630ca65b1a6aeb0a2df79e2926d3a
Annelle <gghaslam@gmail.com>
2023-03-03 13:42:20 -0500

    corrected code in seeds.js and data correction

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -16,7 +16,7 @@ db.once('open', async () => {
   const sitters = await Sitters.insertMany(sittersData);
   const reviews = await Reviews.insertMany(reviewsData);
 
-  sitters.forEach(sitter,index => {
+  sitters.forEach((sitter,index) => {
     const review = reviews[index]._id
     sitter.reviews.push(review)
     sitter.save()
