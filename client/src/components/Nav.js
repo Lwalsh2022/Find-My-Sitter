957ab6d5057fcb7541c649dc5e74d971b4a07d44
Lisa Walsh <lwalshphotography@yahoo.com>
2023-03-06 19:32:43 -0500

    updated sign up form

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,15 +10,8 @@ function Nav(){
         <div>
             <div id='navBar'>
   <a href="/" className="site-title">Find A Sitter</a>
-<<<<<<< HEAD
-  <Link to={{pathname: `/`}} className="w3-bar-item w3-button App-link">Home</Link>
-  <ion-icon name="home-outline"></ion-icon>
-  <Link to={{pathname: `/sitters`}} className="w3-bar-item w3-button App-link">SittersPage</Link>
-  <ion-icon name="man-outline"></ion-icon>
-=======
   <Link to={{pathname: `/`}} className="headerImage"><img src={HomeImg} alt="Home Image"></img></Link>
   <Link to={{pathname: `/sitters`}} className="headerImage"><img src={SitterImg} alt="babysitter image"></img></Link>
->>>>>>> 647dc7213136405e9236d2dffdf75b2d902bca13
             </div>
         </div>
     )
