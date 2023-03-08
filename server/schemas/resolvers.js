6169785bf8a427a802bfac3d935d85e753f3642b
Annelle <gghaslam@gmail.com>
2023-03-08 18:20:55 -0500

    login/signup values to landing page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Post } = require('../models');
 
 
@@ -19,9 +20,26 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { fullName, email, userName }) => {
-      return await User.create({ fullName, email, userName });
+    addUser: async (parent, { name, email, password }) => {
+      return await User.create({ name, email, password });
     },
+
+    login: async (parent, {email, password}) => {
+       const user = await User.findOne({email});
+
+       if (!user) {
+        throw new AuthenticationError("No profile with this email found!")
+       }
+
+       const correctPw = await user.isCorrectPassword(password);
+        
+       if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+       }
+
+       const token = signToken(user);
+       return { token, user};
+      },
 
     updateUser: async (parent, { id, fullName, email, userName }) => {
       return await User.findOneAndUpdate(
