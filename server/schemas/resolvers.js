deb5a30c60a3638dd9759389b989943582aabe95
Matt Lahey <mdlahey@outlook.com>
2023-03-07 19:02:28 -0500

    updated typeDefs and Resolvers with simplified models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,76 +1,64 @@
-const { Parents, Reviews, Sitters } = require('../models');
+const { User, Post } = require('../models');
 
 
 const resolvers = {
  
   Query: {
-    parents: async () => {
-      return Parents.find();
+    users: async () => {
+      return await User.find({}).populate('posts');
     },
-    sitters: async () => {
-      return Sitters.find();
+    user: async(parent, args) => {
+      return await User.findById(args.id);
     },
-    reviews: async () => {
-      return Reviews.find().populate('sitter').populate('parent');
+    posts: async () => {
+      return await Post.find({});
     },
+    post: async(parent,args) => {
+      return await Post.findbyId(args.id)
+    }
   },
 
   Mutation: {
-    addParent: async (parent, args) => {
-      return Parents.create(args);
-    },
-    addSitter: async (parent, args) => {
-      return Sitters.create(args);
-    },
-    addReview: async (_, args) => {
-      console.log('new review information', args)
-      const parent = await Parents.findOne({_id: args.parentId})
-      const sitter = await Sitters.findOne({_id: args.sitterId})
-      console.log('parent', parent)
-      console.log('sitter', sitter)
-      return Reviews.create({...args, parent, sitter});
-    },
-    updateParent: async (parent, args) => {
-      return Parents.findOneAndUpdate(
-        { _id: args._id },
-        {...args},
-        { new: true }
-      );
-    },
-    updateSitter: async (parent, args) => {
-      return Sitters.findOneAndUpdate(
-        { _id: args._id },
-        {...args},
-        { new: true }
-      );
-    },
-    updateReview: async (parent, args) => {
-      return Reviews.findOneAndUpdate(
-        { _id: args._id },
-        {...args},
-        { new: true }
-      );
-    },
-    deleteParent: async (parent, args) => {
-      return Parents.findOneAndDelete({ _id: args._id });
-    },
-    deleteSitter: async (parent, args) => {
-      return Sitters.findOneAndDelete({ _id: args._id });
-    },
-    deleteReview: async (parent, args) => {
-      return Reviews.findOneAndDelete({ _id: args._id });
+    addUser: async (parent, { fullName, email, userName }) => {
+      return await User.create({ fullName, email, userName });
     },
 
-    attachChildrentoParent: async (parent, args) => {
-      return Parents.findOneAndUpdate(
-        { _id: args.parentId },
-        { $push: { children: args.children } },
+    updateUser: async (parent, { id, fullName, email, userName }) => {
+      return await User.findOneAndUpdate(
+        { _id: id },
+        { fullName },
+        { email },
+        { userName },
         { new: true }
       );
+    },
+
+    deleteUser: async(parent, { id }) => {
+      return await User.findOneAndDelete({ id });
+    },
+
+    addPost: async (parent, { postTitle, postText, email, hourlyRate, numberOfChildren, specialNeeds }) => {
+      return await Post.create({ postTitle, postText, email, hourlyRate, numberOfChildren, specialNeeds});
+    },
+
+    updatePost: async (parent, { id, postTitle, postText, email, hourlyRate, numberOfChildren, specialNeeds}) => {
+      return await Post.findOneAndUpdate(
+        { _id: id },
+        { postTitle},
+        { postText },
+        { email },
+        { hourlyRate },
+        { numberOfChildren },
+        { specialNeeds },
+        { new: true }
+      );
+    },
+
+    deletePost: async (parent, { id }) => {
+      return await Post.findOneAndDelete({ id });
     }
 
-  },
-  
+  }
 
 };
 
